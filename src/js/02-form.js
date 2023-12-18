13050c452c761e10c0aedb9ca574339cf1d9019e
Denysit <141247457+Denysit@users.noreply.github.com>
2023-12-18 19:04:37 +0200

    update

---
+++ src/js/02-form.js
@@ -0,0 +1,65 @@
+
+const section = document.querySelector('section');
+
+const newSection = `<form class="feedback-form" autocomplete="off">
+  <label>
+    Email
+    <input type="email" name="email" autofocus />
+  </label>
+  <label>
+    Message
+    <textarea name="message" rows="8"></textarea>
+  </label>
+  <button type="submit">Submit</button>
+</form>`;
+
+section.insertAdjacentHTML('beforeend', newSection);
+
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('[name="email"]');
+const textarea = form.querySelector('[name="message"]');
+
+
+const initialData = localStorage.getItem('feedback-form-state');
+
+if (initialData) {
+  const initialFormData = JSON.parse(initialData);
+  email.value = initialFormData.email;
+  textarea.value = initialFormData.message;
+};
+
+form.addEventListener('input', () => {
+
+ const formData = {
+    email: email.value.trim(),
+    message: textarea.value.trim()
+  };
+
+localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  
+});
+
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  
+  event.preventDefault();
+
+  const emailValue = email.value.trim();
+  const messageValue = textarea.value.trim();
+
+  if (emailValue === '' || messageValue === '') {
+    alert('Будь ласка, заповніть обидва поля форми.');
+    return;
+  }
+ 
+  else {
+    console.log({ email: emailValue, message: messageValue })
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+
+  };
+
+};
+
