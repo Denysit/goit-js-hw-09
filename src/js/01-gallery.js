f67442313ed499516edc313fe450d89fb498fc2d
Denysit <141247457+Denysit@users.noreply.github.com>
2023-12-21 02:33:39 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -83,12 +83,14 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import "../css/gallery-styles.css";
 import '../css/custom-style-modal.css';
-import './img/sprite-modal-sprite-icon.svg';
+import iconsUrl from '../img/sprite-modal-sprite-icon.svg';
 
 
-const iconsUrl = './img/sprite-modal-sprite-icon.svg';
-
-new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250,
+new SimpleLightbox('.gallery a', {
+  
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
 
 nav: true,
   navText: [
