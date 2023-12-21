fe9236b057030f5fe4363f00fd9263770ea62b73
Denysit <141247457+Denysit@users.noreply.github.com>
2023-12-21 21:46:37 +0000

    Deploying to gh-pages from @ Denysit/goit-js-hw-09@1cf2be5ea37792fc53b6e259095e991738f605ed 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";const s=document.querySelector("section"),n=`<form class="feedback-form" autocomplete="off">
+import"./assets/modulepreload-polyfill-ec808ebb.js";const m=document.querySelector("section"),n=`<form class="feedback-form" autocomplete="off">
   <label>
     Email
     <input type="email" name="email" autofocus />
@@ -8,5 +8,5 @@ import"./assets/modulepreload-polyfill-ec808ebb.js";const s=document.querySelect
     <textarea name="message" rows="8"></textarea>
   </label>
   <button type="submit">Submit</button>
-</form>`;s.insertAdjacentHTML("beforeend",n);const t=document.querySelector(".feedback-form"),a=t.querySelector('[name="email"]'),o=t.querySelector('[name="message"]'),m=localStorage.getItem("feedback-form-state");if(m)try{const e=JSON.parse(m);a.value=e.email,o.value=e.message}catch(e){console.error("LOCAL STORAGE PARSE ERROR",e)}t.addEventListener("input",()=>{const e={email:a.value.trim(),message:o.value.trim()};localStorage.setItem("feedback-form-state",JSON.stringify(e))});t.addEventListener("submit",c);function c(e){e.preventDefault();const r=a.value.trim(),l=o.value.trim();if(r===""||l===""){alert("Будь ласка, заповніть обидва поля форми.");return}else console.log({email:r,message:l}),localStorage.removeItem("feedback-form-state"),t.reset()}
+</form>`;m.insertAdjacentHTML("beforeend",n);const t=document.querySelector(".feedback-form"),a=t.querySelector('[name="email"]'),o=t.querySelector('[name="message"]'),r=localStorage.getItem("feedback-form-state");if(r)try{const e=JSON.parse(r);a.value=e.email,o.value=e.message}catch(e){console.error("LOCAL STORAGE PARSE ERROR",e)}t.addEventListener("input",()=>{const e={email:a.value.trim(),message:o.value.trim()};localStorage.setItem("feedback-form-state",JSON.stringify(e))});t.addEventListener("submit",i);function i(e){e.preventDefault();const l=a.value,s=o.value;if(l===""||s===""){alert("Please fill in both fields in the form.");return}console.log({email:l,message:s}),localStorage.removeItem("feedback-form-state"),t.reset()}
 //# sourceMappingURL=commonHelpers2.js.map
